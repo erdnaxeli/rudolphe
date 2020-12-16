9ff0ef21ac574acb1fca01d66c5f2cd11df61634
Alexandre Morignot <amorignot@meilleursagents.com>
2020-12-16 17:28:18 +0100

    Show new points when a new part is completed

--- src/rudolphe.cr
+++ src/rudolphe.cr
@@ -24,7 +24,9 @@ module Rudolphe
       Log.info { "Checking leaderboard" }
       aoc.get_leaderboard.try &.users.each do |user_id, user|
         if db_user = db_leaderboard.users[user_id]?
+          new_points = nil
           if user.local_score != db_user.local_score
+            new_points = user.local_score - db_user.local_score
             repository.save_user_local_score(user)
           end
 
@@ -32,7 +34,7 @@ module Rudolphe
             if db_day = db_user.days[day]?
               parts.each_key do |part|
                 if !db_day.has_key?(part)
-                  matrix.send("#{user.name} vient juste de compléter la partie #{part} du jour #{day}")
+                  matrix.send("#{user.name} vient juste de compléter la partie #{part} du jour #{day} (+#{new_points} points)")
                   repository.save_user_part(user_id, day, part, parts[part])
                 end
               end
@@ -43,7 +45,7 @@ module Rudolphe
                 parts_msg = "la partie #{parts.first_key}"
               end
 
-              matrix.send("#{user.name} vient juste de compléter #{parts_msg} du jour #{day}")
+              matrix.send("#{user.name} vient juste de compléter #{parts_msg} du jour #{day} (+#{new_points} points)")
               repository.save_user_day(user_id, day, parts)
             end
           end
