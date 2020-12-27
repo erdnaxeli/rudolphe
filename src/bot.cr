ff23a2bf2afc6fdc593404fa26c28d5bd1d4894f
Alexandre Morignot <amorignot@meilleursagents.com>
2020-12-27 18:10:19 +0100

    Bump marmot to v0.3.0

--- src/bot.cr
+++ src/bot.cr
@@ -18,10 +18,10 @@ class Rudolphe::Bot
     @db_leaderboard = @repository.get_leaderboard
 
     @quiet = false
-    @leaderboard_task = Marmot.repeat(15.minutes, true) { check_leaderboard }
+    @leaderboard_task = Marmot.every(15.minutes, true) { check_leaderboard }
 
     # The new puzzle is out on midnight UTC-5, which is 6:00 at UTC+1
-    @puzzle_task = Marmot.cron(6, 0) { send_puzzle_link }
+    @puzzle_task = Marmot.every(:day, hour: 6) { send_puzzle_link }
 
     Marmot.run
   end
@@ -73,13 +73,13 @@ class Rudolphe::Bot
       @quiet = true
       @puzzle_task.cancel
       @leaderboard_task.cancel
-      @leaderboard_task = Marmot.repeat(1.hour) { check_leaderboard }
+      @leaderboard_task = Marmot.every(1.hour) { check_leaderboard }
     elsif @quiet && now.month == 12 && (1..25).includes?(now.day)
       Log.info { "Going into competiton mode" }
       @quiet = false
-      @puzzle_task = Marmot.cron(6, 0) { send_puzzle_link }
+      @puzzle_task = Marmot.every(:day, hour: 6) { send_puzzle_link }
       @leaderboard_task.cancel
-      @leaderboard_task = Marmot.repeat(15.minutes) { check_leaderboard }
+      @leaderboard_task = Marmot.every(15.minutes) { check_leaderboard }
     end
   end
 
